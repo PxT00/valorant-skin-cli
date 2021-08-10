1e1a4ed359d21a40b56e557bfd4ad435f25b91d3
PxT00 <65025260+PxT00@users.noreply.github.com>
2021-08-10 11:51:02 -0300

    Update version_checker.py

--- src/utility/version_checker.py
+++ src/utility/version_checker.py
@@ -9,4 +9,4 @@ class Checker:
             data = requests.get("https://api.github.com/repos/colinhartigan/valorant-skin-cli/releases/latest")
             latest = data.json()["tag_name"]
             if latest != current_version:
-                color_print([("Yellow bold",f"an update is available ({current_version} -> {latest})! download it at "),("Cyan underline",f"https://github.com/colinhartigan/valorant-skin-cli/releases/tag/{latest}")])
+                color_print([("Yellow bold",f"uma atualização está disponível! ({current_version} -> {latest})! baixe em: "),("Cyan underline",f"https://github.com/colinhartigan/valorant-skin-cli/releases/tag/{latest}")])
